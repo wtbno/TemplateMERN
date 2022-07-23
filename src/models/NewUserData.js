d93088f7db1208651373027413da774654e687a6
Bruno Alves <bruno_links@hotmail.com>
2022-07-23 17:05:16 -0300

    hash bcrypt

--- src/models/NewUserData.js
+++ src/models/NewUserData.js
@@ -1,12 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const NewUserDataSchema = new mongoose.Schema({
+const NewUserDataSchema = new mongoose.Schema(
+  {
     name: String,
     email: String,
     password: String,
-    birthDate: String
+    birthDate: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+NewUserDataSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    return next();
+  }
+  this.password = bcrypt.hashSync(this.password, 10);
 });
-
-
-
-module.exports = mongoose.model('Novo_usuario', NewUserDataSchema)
+module.exports = mongoose.model("Novo_usuario", NewUserDataSchema);
